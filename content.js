c6cde5be906c70b6f2397a4a8442584ca2160bce
Masataka Yakura <masataka.yakura+github@gmail.com>
2013-01-15 10:38:49 +0900

    ver 0.2: include support for Mozilla changeset.

--- content.js
+++ content.js
@@ -1,5 +1,14 @@
 (function () {
-    var revision = document.querySelector('#log td:first-of-type').textContent.trim(),
-        summary  = document.querySelector('#log td:nth-of-type(3)').textContent.trim();
-    document.title += ' \u2014 ' + revision + '. ' + summary;
+    var url = location.href;
+    // HTML5 Tracker
+    if (/http:\/\/html5\.org\/tools\/web-apps-tracker/.test(url)) {
+        var revision = document.querySelector('#log td:first-of-type').textContent.trim(),
+            summary  = document.querySelector('#log td:nth-of-type(3)').textContent.trim();
+        document.title += ' \u2014 ' + revision + '. ' + summary;
+    }
+    // Mozilla changeset
+    if (/http:\/\/hg\.mozilla\.org\//.test(url)) {
+        var summary = document.querySelector('.title').textContent.trim();
+        document.title += ' \u2014 ' + summary;
+    }
 }());
