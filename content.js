c4feeb3c2f72cdda17abd5e0d5c27c02c91ae865
Masataka Yakura <masataka.yakura+github@gmail.com>
2013-01-15 10:28:43 +0900

    oops, not x

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 (function () {
     var revision = document.querySelector('#log td:first-of-type').textContent.trim(),
         summary  = document.querySelector('#log td:nth-of-type(3)').textContent.trim();
-    document.title += ' \x2014 ' + revision + '. ' + summary;
+    document.title += ' \u2014 ' + revision + '. ' + summary;
 }());
