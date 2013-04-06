4cde8cfa63d0375830413f82ec3db21455545eb9
Masataka Yakura <masataka.yakura@gmail.com>
2013-04-06 17:36:53 +0900

    Add googlesource.com rule for Blink changesets.

--- content.js
+++ content.js
@@ -11,4 +11,16 @@
         var summary = document.querySelector('.title').textContent.trim();
         document.title += ' \u2014 ' + summary;
     }
+    // googlesource.com (mainly targeting Blink)
+    if (/https?:\/\/.*\.googlesource\.com\/.+\/\+\/[0-9a-f]{5,40}/.test(url)) {
+        // title structure is [hash] - [repo/dir] - Git at Google
+        // e.g. 4b7159b920caefd3bc8d48a35daddd9bbac862dc - chromium/blink - Git at Google
+        // turning into [shorthash] (@[revision]) - [repo/dir] -- [summary]
+        var message = document.querySelector('.commit-message').textContent,
+            revision = /git-svn-id: svn:\/\/.*@(\d+)/.test(message) ? message.match(/git-svn-id: svn:\/\/.*@(\d+)/)[1] : null,
+            summary = message.match(/(.*)\n/)[1];
+        var title = document.title,
+            titlereg = /([0-9a-f]+) - (.+?) /;
+        document.title = titlereg.exec(title)[1].slice(0,10) + (!!revision ? ' (@' + revision + ') ' : '') + ' - ' + titlereg.exec(title)[2] + ' \u2014 ' + summary;
+    }
 }());
