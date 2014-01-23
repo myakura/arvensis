ff4531f0e93e326b27aa3a6a70b44750a3931825
Masataka Yakura <masataka.yakura@gmail.com>
2014-01-23 19:12:52 +0900

    support HTTPS.

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 (function () {
     var url = location.href;
     // HTML5 Tracker
-    if (/http:\/\/html5\.org\/tools\/web-apps-tracker/.test(url)) {
+    if (/https?:\/\/html5\.org\/tools\/web-apps-tracker/.test(url)) {
         var revision = document.querySelector('#log td:first-of-type').textContent.trim(),
             summary  = document.querySelector('#log td:nth-of-type(3)').textContent.trim();
         document.title += ' \u2014 ' + revision + '. ' + summary;
     }
     // Mercurial changeset
-    if (/http:\/\/hg\.mozilla\.org\/|https:\/\/dvcs\.w3\.org\/hg\/.*\/rev/.test(url)) {
+    if (/https?:\/\/hg\.mozilla\.org\/|https?:\/\/dvcs\.w3\.org\/hg\/.*\/rev/.test(url)) {
         var summary = document.querySelector('.title').textContent.trim();
         document.title += ' \u2014 ' + summary;
     }
