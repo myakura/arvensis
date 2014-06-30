652509cfb6dcea352dd946b1513b90692dbc02dc
Masataka Yakura <masataka.yakura@gmail.com>
2014-07-01 00:24:55 +0900

    support html5.org/r/* URLs.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 (function () {
     var url = location.href;
     // HTML5 Tracker
-    if (/https?:\/\/html5\.org\/tools\/web-apps-tracker/.test(url)) {
+    if (/https?:\/\/html5\.org\/tools\/web-apps-tracker|https?:\/\/html5\.org\/r\/\d+/.test(url)) {
         var revision = document.querySelector('#log td:first-of-type').textContent.trim(),
             summary  = document.querySelector('#log td:nth-of-type(3)').textContent.trim();
         document.title += ' \u2014 ' + revision + '. ' + summary;
