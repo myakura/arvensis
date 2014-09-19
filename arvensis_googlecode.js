4607b77813e23f0979b8604b5754bcc4f5f8a8c3
Masataka Yakura <masataka.yakura@gmail.com>
2014-09-19 22:17:14 +0900

    fix bug where commit message is in a single line.
    it failed on https://code.google.com/p/v8/source/detail?r=24073

--- arvensis_googlecode.js
+++ arvensis_googlecode.js
@@ -3,5 +3,5 @@
 var pname = document.querySelector('#pname span').textContent.trim()
 var revision = document.querySelector('#colcontrol div:nth-last-of-type(2) b:last-child').textContent.trim()
 var message = document.querySelector('.wrap').textContent.trim()
-var summary = /^(.+)\n/.exec(message)[1]
+var summary = /^(.+)\n?/.exec(message)[1]
 document.title = revision + ' - ' + pname + ' \u2014 ' + summary
