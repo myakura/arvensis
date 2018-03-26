a6fc077482ec91c417494376d01268760aa10606
Masataka Yakura <masataka.yakura@gmail.com>
2018-03-26 20:54:10 +0900

    remove html5org script since the page moved to github

--- arvensis_html5org.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-var revision = document.querySelector('#log td:first-of-type').textContent.trim()
-var summary = document.querySelector('#log td:nth-of-type(3)').textContent.trim();
-document.title += ' \u2014 ' + revision + '. ' + summary
